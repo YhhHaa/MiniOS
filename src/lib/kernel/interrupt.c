92076c878d751874b4589aec1240b8f84c045b1a
yetao <2168929065@qq.com>
2022-01-16 21:16:41 +0800

    初始化8295A

--- src/lib/kernel/interrupt.c
+++ src/lib/kernel/interrupt.c
@@ -1,8 +1,13 @@
 #include "../../include/kernel/interrupt.h"
 #include "../../include/kernel/stdint.h"
 #include "../../include/kernel/global.h"
+#include "../../include/kernel/io.h"
 
 
+#define PIC_M_CTRL 0x20 // 主片控制端口
+#define PIC_M_DATA 0x21 // 主片数据端口
+#define PIC_S_CTRL 0xa0 // 从片控制端口
+#define PIC_S_DATA 0xa1 // 从片数据端口
 #define IDT_DESC_CNT 0x21 // 目前支持的中断数
 
 
@@ -40,6 +45,28 @@ static void idt_desc_init(void) {
         make_idt_desc(&idt[i], IDT_DESC_ATTR_DPL0, intr_entry_table[i]);
     }
     put_str("idt_desc_init done\n");
+}
+
+
+// 初始化可编程中断控制器8295A
+static void pic_init(void) {
+    // 初始化主片
+    outb(PIC_M_CTRL, 0x11); // ICW1: 边沿触发, 级联模式, 需要ICW4
+    outb(PIC_M_DATA, 0x20); // ICW2: 起始中断向量号为0x20
+    outb(PIC_M_DATA, 0x04); // ICW3: IR2接从片
+    outb(PIC_M_DATA, 0x01); // ICW4: 8086模式, 正常EOI
+
+    // 初始化从片
+    outb(PIC_S_CTRL, 0x11); // ICW1: 边沿触发, 级联模式, 需要ICW4
+    outb(PIC_S_DATA, 0x28); // ICW2: 起始中断向量号为0x28
+    outb(PIC_S_DATA, 0x02); // ICW3: 设置从片连接到主片的IR2引脚
+    outb(PIC_S_DATA, 0x01); // ICW4: 8086模式, 正常EOI
+
+    // 打开主片上IRO, 只接受时钟产生的中断
+    outb(PIC_M_DATA, 0xfe); // 主片OCW1: 不屏蔽时钟中断, 屏蔽其他中断
+    outb(PIC_S_DATA, 0xff); // 从片OCW1: 全部屏蔽中断
+
+    put_str("pic_init done\n");
 }
 
 // 完成有关中断的所有初始化操作
