51f34bf534755de9c9cf8ba8833ac09407325f54
yetao <2168929065@qq.com>
2022-01-18 19:27:07 +0800

    开关中断

--- src/lib/kernel/interrupt.c
+++ src/lib/kernel/interrupt.c
@@ -9,6 +9,8 @@
 #define PIC_S_CTRL 0xa0 // 从片控制端口
 #define PIC_S_DATA 0xa1 // 从片数据端口
 #define IDT_DESC_CNT 0x21 // 目前支持的中断数
+#define EFLAGS_IF 0x00000200
+#define GET_EFLAGS(EFLAG_VAR) asm volatile("pushfl; popl %0": "=g"(EFLAG_VAR))
 
 
 // 中断门描述符结构体
@@ -111,6 +113,41 @@ static void exception_init(void) {
     intr_name[17] = "#AC Alignment Check Exception";
     intr_name[18] = "#MC Machine-Check Exception";
     intr_name[19] = "#XF SIMD Floating-Point Exception";
+}
+
+
+enum intr_status intr_enable() {
+    enum intr_status old_status;
+    if (INTR_ON == intr_get_status()) {
+        old_status = INTR_ON;
+        return old_status;
+    } else {
+        old_status = INTR_OFF;
+        asm volatile("sti");
+        return old_status;
+    }
+}
+
+enum intr_status intr_disable() {
+    enum intr_status old_status;
+    if (INTR_ON == intr_get_status()) {
+        old_status = INTR_ON;
+        asm volatile("cli": : : "memory");
+        return old_status;
+    } else {
+        old_status = INTR_OFF;
+        return old_status;
+    }
+}
+
+enum intr_status intr_set_status(enum intr_status status) {
+    return status & INTR_ON ? intr_enable(): intr_disable();
+}
+
+enum intr_status intr_get_status() {
+    uint32_t eflags = 0;
+    GET_EFLAGS(eflags);
+    return (EFLAGS_IF & eflags)? INTR_ON: INTR_OFF;
 }
 
 // 完成有关中断的所有初始化操作
