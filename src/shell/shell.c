a3fd0024fa756edf5f192f17642798c8ddfae3d8
yetao <2168929065@qq.com>
2022-02-16 14:00:07 +0800

    解析命令

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -62,7 +62,7 @@ static void readline(char* buf, int32_t count) {
 				*(pos--) = 0;
 			}
 			break;
-			
+
 		/* 非控制键则输出字符 */
 		default:
 			putchar(*pos);
@@ -71,17 +71,81 @@ static void readline(char* buf, int32_t count) {
 	}
 	printf("readline: can`t find enter_key in the cmd_line, max num of char is 128\n");
 }
+
+/* 分析字符串cmd_str中以token为分隔符的单词,将各单词的指针存入argv数组 */
+static int32_t cmd_parse(char* cmd_str, char** argv, char token) {
+	ASSERT(cmd_str != NULL);
+	/* 1. 清空数组argv */
+	int32_t arg_idx = 0;
+	while(arg_idx < MAX_ARG_NR) {
+		argv[arg_idx] = NULL;
+		arg_idx++;
+	}
+
+	/* 2. 处理命令的每一个字符, 存入argv数组 */
+	char* next = cmd_str;
+	int32_t argc = 0;
+	/* 外层循环处理整个命令行 */
+	while(*next) {
+		/* 去除命令字或参数之间的空格 */
+		while(*next == token) {
+			next++;
+		}
+		/* 处理最后一个参数后接空格的情况,如"ls dir2 " */
+		if (*next == 0) {
+			break; 
+		}
+
+		argv[argc] = next;
+		/* 内层循环处理命令行中的每个命令字及参数 */
+		while (*next && *next != token) {	  // 在字符串结束前找单词分隔符
+			next++;
+		}
+	
+		/* 如果未结束(是token字符),使tocken变成0 */
+		if (*next) {
+			*next++ = 0;	// 将token字符替换为字符串结束符0,做为一个单词的结束,并将字符指针next指向下一个字符
+		}
+	
+		/* 避免argv数组访问越界,参数过多则返回0 */
+		if (argc > MAX_ARG_NR) {
+			return -1;
+		}
+		argc++;
+	}
+	return argc;
+}
  
+char* argv[MAX_ARG_NR] = {NULL};
+char final_path[MAX_PATH_LEN] = {0};
+int32_t argc = -1;
+
 /* 简单的shell */
 void my_shell(void) {
-	cwd_cache[0] = '/';
-	while (1) {
-		print_prompt(); 
-		memset(cmd_line, 0, cmd_len);
-		readline(cmd_line, cmd_len);
-		if (cmd_line[0] == 0) {	 // 若只键入了一个回车
-		continue;
-		}
-	}
-	PANIC("my_shell: should not be here");
+    cwd_cache[0] = '/';
+    while (1) {
+        print_prompt(); 
+        memset(final_path, 0, MAX_PATH_LEN);
+        memset(cmd_line, 0, MAX_PATH_LEN);
+        readline(cmd_line, MAX_PATH_LEN);
+        if (cmd_line[0] == 0) {	 // 若只键入了一个回车
+	        continue;
+        }
+ 
+        argc = -1;
+	    argc = cmd_parse(cmd_line, argv, ' ');
+	    if (argc == -1) {
+	        printf("num of arguments exceed %d\n", MAX_ARG_NR);
+	        continue;
+	    }
+ 
+        int32_t arg_idx = 0;
+        while(arg_idx < argc){
+            printf("%s ",argv[arg_idx]);
+            arg_idx++;
+        }
+        printf("\n");
+ 
+    }
+    PANIC("my_shell: should not be here");
 }
